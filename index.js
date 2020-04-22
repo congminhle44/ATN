aa2383893d52291fe4df9b0d231280513023f7a1
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2020-04-22 14:37:37 +0700

    second commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+// viewed at http://localhost:8080
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname + "/Template/Asset/Public/login.html"));
+});
+app.get("/order", function (req, res) {
+  res.sendFile(path.join(__dirname + "/Template/Asset/Public/order.html"));
+});
+app.get("/profile", function (req, res) {
+  res.sendFile(path.join(__dirname + "/Template/Asset/Public/profile.html"));
+});
+app.listen(8080);
