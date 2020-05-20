61b674f238d5ea86acaab5189e3dfc0429ddd29b
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2020-05-20 22:24:03 +0700

    commit

--- public/Service/product-report.js
+++ public/Service/product-report.js
@@ -15,22 +15,21 @@ function getProductList() {
       return err;
     });
 }
-
 renderProductTable = (product) => {
   let productTable = getEle("product-table");
   let content = "";
   product.map((item, index) => {
     content += `
-              <tr>
-              <td>${item.maSP}</td>
-              <td>${item.tenSP}</td>
-              <td>${item.giaTien}</td>
-              <td>${item.ngayBan}</td>
-              <td>
-              <button class="btn btn-danger" onClick="delProduct(${item.maSP})">Xóa đơn hàng</button>
-              </td>
-              </tr>
-          `;
+                <tr>
+                <td>${item.maSP}</td>
+                <td>${item.tenSP}</td>
+                <td>${item.giaTien}</td>
+                <td>${item.ngayBan}</td>
+                <td>
+                <button class="btn btn-danger" onClick="delProduct(${item.maSP})">Xóa đơn hàng</button>
+                </td>
+                </tr>
+            `;
   });
   return (productTable.innerHTML = content);
 };
